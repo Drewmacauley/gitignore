7541d7c5311e2342acaf45d112007352214d6b5f
drewparker <drewjparker91@gmail.com>
2020-08-13 13:51:12 -0700

    Fix js block structure
    Co-authored-by: William DS <wdstwo@live.com>
    Co-authored-by: Andrew Macauley <macauleyandy03@gmail.com

--- js/scripts.js
+++ js/scripts.js
@@ -9,13 +9,15 @@ $(document).ready(function() {
     const side3 = parseInt($("input#side3").val());
 
     if (side1 === side2 && side2 === side3) {
-      $("results").text("equilateral"); 
-    } else if (side1 === side2 && side2 !== side3 || side2 === side3 && side1 !== side2) || side1 === side3 &&  side2 !== side3) {
-      $("results").text("isosceles");
+      $("#results").text("equilateral"); 
+    } else if (add(side1, side2) <= side3 || add(side1, side3) <= side2 || add(side2, side3) <= side1) {
+      $("#results").text("NOT a triangle");
+    } else if (side1 === side2 && side2 !== side3 || side2 === side3 && side1 !== side2 || side1 === side3 &&  side2 !== side3) {
+      $("#results").text("isosceles");
     } else if (side1 !== side2 && side2 !== side3) {
-      $("results").text("scalene");
-    } else if (add(side1,side2) <= side3 || add(side1, side3) <= side2 || add(side2, side3) <= side1) {
-      $("results").text("NOT a triangle");
+      $("#results").text("scalene");
     }
+    
+    event.preventDefault();
   }); 
 });
